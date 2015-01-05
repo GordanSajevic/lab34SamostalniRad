c562d1f834eeda9e81c107cce0b313674e8ee008
Gordan Sajević <gsajevic@hotmail.com>
2015-01-05 22:17:55 +0100

    Added comments

--- src/lab34/Pogadjanje.java
+++ src/lab34/Pogadjanje.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Pogadjanje {
 
 	//Samostalni rad
+	//Igra pogađanje broja. Program "zamisli" broj između 1 i 100, a korisnik pogađa broj, dok ga program navodi
+	//tako što javlja da li je uneseni broj veći ili manji od zamišljenog
 	
 	public static void main(String[] args) {
 		Scanner unos = new Scanner(System.in);
