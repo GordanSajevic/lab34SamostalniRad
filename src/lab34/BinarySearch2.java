dbcf2864aba6630f77819bc6a1441fa94010d12c
Gordan Sajević <gsajevic@hotmail.com>
2015-01-05 13:53:20 +0100

    added BinarySearch2

---
+++ src/lab34/BinarySearch2.java
@@ -0,0 +1,43 @@
+package lab34;
+
+import java.util.Scanner;
+
+public class BinarySearch2 {
+	
+	public static int binarySearchLoop(int[] array, int number)
+	{
+		int start = 0;
+		int end = array.length;
+		int mid;
+		while (start < end)
+		{
+			mid = (start+end)/2;
+			if (array[mid] == number)
+			{
+				return mid;
+			}
+			else if (array[mid] > number)
+			{
+				end = mid;
+			}
+			else
+			{
+				start = ++mid;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner unos = new Scanner(System.in);
+		int[] array = new int [10];
+		System.out.println("Enter number: ");
+		int num = unos.nextInt();
+		for (int i=0; i<10; i++)
+		{
+			array[i] = i+1;
+		}
+		System.out.println(binarySearchLoop(array, num));
+	}
+
+}
